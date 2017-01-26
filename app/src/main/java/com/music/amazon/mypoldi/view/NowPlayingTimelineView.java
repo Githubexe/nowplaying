3450f61911317a4a0cd8145f814781a0a411a8f8
githubexe <githubexe@gmail.com>
2017-01-25 17:09:08 -0800

    - made use of RecyclerView for the timeline view

--- app/src/main/java/com/music/amazon/mypoldi/view/NowPlayingTimelineView.java
+++ app/src/main/java/com/music/amazon/mypoldi/view/NowPlayingTimelineView.java
@@ -1,11 +1,14 @@
 package com.music.amazon.mypoldi.view;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.music.amazon.mypoldi.GameEventAdapter;
 import com.music.amazon.mypoldi.R;
 
 /**
@@ -13,22 +16,15 @@ import com.music.amazon.mypoldi.R;
  */
 public final class NowPlayingTimelineView extends RelativeLayout {
 
+    private Context context;
+
     public TextView scoreTextView;
 
     public TextView timeTextView;
 
-    public ImageView leftImage;
-
-    public TextView leftText;
-
-    public ImageView rightImage;
-
-    public TextView rightText;
-
 
     public NowPlayingTimelineView(Context context) {
         this(context, null);
-
     }
 
     public NowPlayingTimelineView(Context context, AttributeSet attributeSet) {
@@ -37,19 +33,20 @@ public final class NowPlayingTimelineView extends RelativeLayout {
 
     public NowPlayingTimelineView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
         init();
     }
 
     private void init() {
-        inflate(getContext(), R.layout.now_playing_timeline_view, this);
+        inflate(getContext(), R.layout.now_playing_game_event_recycler_view, this);
         scoreTextView = (TextView)findViewById(R.id.now_playing_score_text);
         timeTextView = (TextView)findViewById(R.id.now_playing_time_text);
 
-        leftImage = (ImageView)findViewById(R.id.leftCard);
-        leftText = (TextView)findViewById(R.id.leftText);
-        rightImage = (ImageView)findViewById(R.id.rightCard);
-        rightText = (TextView) findViewById(R.id.rightText);
-    }
 
+        RecyclerView rv = (RecyclerView) findViewById(R.id.game_event_recycler_view);
+        GameEventAdapter adapter = new GameEventAdapter(context);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager((context)));
+    }
 
 }
