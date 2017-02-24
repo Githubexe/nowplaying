9c404228cb0956908c094e7f4834a0ee3b3dabdc
Su, Yoyo <yoyosu@amazon.com>
2017-02-23 18:30:32 -0800

    - Fixed crash

--- app/src/main/java/com/music/amazon/mypoldi/view/LiveFeedItemView.java
+++ app/src/main/java/com/music/amazon/mypoldi/view/LiveFeedItemView.java
@@ -40,30 +40,39 @@ public abstract class LiveFeedItemView extends LinearLayout {
     }
 
     public void bind(final LiveFeedItemModel liveFeedItemModel) {
-        timeTextView.setText(liveFeedItemModel.time);
-        commentTextView.setText(liveFeedItemModel.description);
-        commentTextView.bringToFront();
+        if (liveFeedItemModel.time.isPresent()) {
+            timeTextView.setText(liveFeedItemModel.time.get());
+        }
+
+        if (liveFeedItemModel.comment.isPresent()) {
+            commentTextView.setText(liveFeedItemModel.comment.get());
+            commentTextView.bringToFront();
+        }
 
         final Picasso picasso = Picasso.with(getContext());
-        if (StringUtils.isEmpty(liveFeedItemModel.smallImage) == false) {
-            picasso.load(liveFeedItemModel.smallImage)
-                    .placeholder(R.drawable.ic_goal_l)
-                    .error(R.drawable.ic_goal_l)
-                    .into(smallImageView);
-            smallImageView.setVisibility(View.VISIBLE);
-        } else {
-            smallImageView.setImageDrawable(null);
-            smallImageView.setVisibility(View.INVISIBLE);
+        if (liveFeedItemModel.smallImage.isPresent()) {
+            if (StringUtils.isEmpty(liveFeedItemModel.smallImage.get()) == false) {
+                picasso.load(liveFeedItemModel.smallImage.get())
+                        .placeholder(R.drawable.ic_goal_l)
+                        .error(R.drawable.ic_goal_l)
+                        .into(smallImageView);
+                smallImageView.setVisibility(View.VISIBLE);
+            } else {
+                smallImageView.setImageDrawable(null);
+                smallImageView.setVisibility(View.INVISIBLE);
+            }
         }
-        if (StringUtils.isEmpty(liveFeedItemModel.largeImage) == false) {
-            picasso.load(liveFeedItemModel.largeImage)
-                    .placeholder(R.drawable.ic_goal_l)
-                    .error(R.drawable.ic_goal_l)
-                    .into(largeImageView);
-            largeImageView.setVisibility(View.VISIBLE);
-        } else {
-            largeImageView.setImageDrawable(null);
-            largeImageView.setVisibility(View.INVISIBLE);
+        if (liveFeedItemModel.largeImage.isPresent()) {
+            if (StringUtils.isEmpty(liveFeedItemModel.largeImage.get()) == false) {
+                picasso.load(liveFeedItemModel.largeImage.get())
+                        .placeholder(R.drawable.ic_goal_l)
+                        .error(R.drawable.ic_goal_l)
+                        .into(largeImageView);
+                largeImageView.setVisibility(View.VISIBLE);
+            } else {
+                largeImageView.setImageDrawable(null);
+                largeImageView.setVisibility(View.INVISIBLE);
+            }
         }
     }
 }
